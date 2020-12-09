d2454b40668f4bbf1c36338f6a826fd01c71298f
Jinjo-bit <71643734+Jinjo-bit@users.noreply.github.com>
2020-12-09 13:35:40 -0500

    Add files via upload

---
+++ Javascript-Lab-Starter-Files-master/JS Output Challenge/script.js
@@ -0,0 +1,27 @@
+// Enter your code below to edit index.html
+
+yourName = "Quez"
+document.getElementById("yourName").innerHTML = yourName;
+
+myBio = "I am a part of the holy crusade"
+document.getElementById("myBio").innerHTML = myBio;
+
+collegeReason = "I wish to go to grace college."
+document.getElementById("collegeReason").innerHTML = collegeReason;
+
+
+careerReason = "i want to be a programming doctor irs agent. the company i want to work for is nintendo"
+document.getElementById("careerReason").innerHTML = careerReason;
+
+
+answerOne = "The DOM represents a document with a logical tree."
+document.getElementById("answerOne").innerHTML = answerOne;
+
+answerTwo = "Writing into an HTML element, using innerHTML "
+"Writing into the HTML output using documentwrite(" 
+"Writing into an alert box, using windowalert(" 
+"Writing into the browser console, using console.log( "
+document.getElementById("answerTwo").innerHTML = answerTwo;
+
+answerThree = "JavaScript can change all the HTML elements in the page"
+document.getElementById("answerThree").innerHTML = answerThree;
